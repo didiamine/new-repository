1db531bd35deff96843ef56cad8859ecc21b442c
didiamine <47870717+didiamine@users.noreply.github.com>
2019-05-09 20:49:47 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,98 @@
-#include "player.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "initialiser.h"
+#define largeur 1000
+#define hauteur 700
 
-int main (void)
+int main(int argc, char *argv[])
 {
-SDL_Surface *ecran =NULL;
-	pers p;
+    int a=1,frame=0,speed=10;
+    bg b;
+    perso pe;
+    int d=0;
 
     SDL_Init(SDL_INIT_VIDEO);
-ecran = SDL_SetVideoMode(1366, 768, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-    SDL_WM_SetCaption("annimation joueur", NULL);
 
-if (ecran==NULL)
-{
-printf("error: %s ",SDL_GetError());
-exit(EXIT_FAILURE);
-}
+    if(!SDL_Init(SDL_INIT_VIDEO))
+    {
+        printf("erreur de chargement de la bibliotheque video : %s",SDL_GetError());
+    }
+
+    SDL_Surface *screen=NULL;
+
+    screen=SDL_SetVideoMode(largeur,hauteur,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
+    if(!screen)
+    {
+        printf("ereur de chargement de l'ecran : %s",SDL_GetError());
+    }
+
+    initialiserbg(&b);
+    initialiserps(&pe);
+    afficherbg(screen,&b);
+    afficherpers(screen,&pe,&frame);
+    SDL_Flip(screen);
+
+    SDL_Event event;
 
 
-initialiserperso( &p);
-mouvement(p,ecran);
-liberer_surface(&p);
-SDL_Quit();
-    return EXIT_SUCCESS;
+    SDL_EnableKeyRepeat(10,10);
+    while(a)
+    {
+       /* afficherbg(screen,&b);
+        afficherpers(screen,&pe,&frame);
+        SDL_Flip(screen);*/
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_KEYDOWN :
+            switch(event.key.keysym.sym)
+            {
+            case SDLK_ESCAPE :
+                a=0;
+                break;
+            case SDLK_RIGHT :
+                animationd(screen,&b,&pe,&frame,&speed);
+                frame++;
+                break;
+            case SDLK_LEFT :
+                animationg(screen,&b,&pe,&frame,&speed);
+                frame++;
+                break;
+            case SDLK_n :
+                accelerationg(screen,&b,&pe,&frame,&speed);
+                frame++;
+                break;
+            case SDLK_m :
+                accelerationd(screen,&b,&pe,&frame,&speed);
+                frame++;
+                break;
+            case SDLK_v :
+                if(frame<3)
+                {
+                    d=1;
+                    if(frame>=3)
+                    {
+                        frame=0;
+                    }
+                    saut(screen,&b,&pe,&frame);
+                }
+
+                if(frame>2||frame<6)
+                {
+                    d=0;
+                    if(frame<=2||frame>=6)
+                    {
+                        frame=3;
+                    }
+                    saut(screen,&b,&pe,&frame);
+                }
+                frame++;
+                break;
+            }
+            break;
+        }
+    }
+return (0);
 }
